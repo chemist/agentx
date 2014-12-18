875faaa5f7a7f0ddbb679cbf2f0538c355b1a4cd
Alexey Smirnov <chemistmail@gmail.com>
2014-12-18 18:06:12 +0400

    fix

--- src/Network/Protocol/Snmp/AgentX/Handlers.hs
+++ src/Network/Protocol/Snmp/AgentX/Handlers.hs
@@ -28,42 +28,8 @@ route p@(Packet _ pdu _ _ _ _) = do
                     GetNext _ srange -> getNextHandler srange
                     _ -> undefined
 
-
 getHandler :: [OID] -> AgentT [MIB]
 getHandler xs = bridgeToBase (findMany xs) 
-
-{--
-7.2.3.2.  Subagent Processing of the agentx-GetNext-PDU
-
-    Upon the subagent's receipt of an agentx-GetNext-PDU, each
-    SearchRange in the request is processed as follows:
-
-    (1)  The subagent searches for a variable within the
-         lexicographically ordered list of variable names for all
-         variables it instantiates (without regard to registration of
-         regions) within the indicated context and session, as follows:
-
-         -  if the "include" field of the starting OID is 0, the
-            variable's name is the closest lexicographical successor to
-            the starting OID.
-
-         -  if the "include" field of the starting OID is 1, the
-            variable's name is either equal to, or the closest
-            lexicographical successor to, the starting OID.
-
-         -  If the ending OID is not null, the variable's name
-            lexicographically precedes the ending OID.
-
-         If a variable is successfully located, v.name is set to that
-         variable's name.  v.type and v.data are encoded to represent the
-         variable's syntax and value, as described in section 5.4, "Value
-         Representation".
-
-    (2)  If the subagent cannot locate an appropriate variable, v.name is
-         set to the starting OID, and the VarBind is set to `
-         endOfMibView', in the manner described in section 5.4, "Value
-         Representation".
---}
 
 getNextHandler :: [SearchRange] -> AgentT [MIB]
 getNextHandler [] = return []
