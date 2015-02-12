9abfcd57c285a798b7b85dd69e065b6901500f48
chemist <chemistmail@gmail.com>
2015-02-12 21:56:34 +0300

    tree

--- src/Network/Protocol/Snmp/AgentX/MIBTree/Operations.hs
+++ src/Network/Protocol/Snmp/AgentX/MIBTree/Operations.hs
@@ -1,121 +1,46 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Network.Protocol.Snmp.AgentX.MIBTree.Operations where
 
-import Control.Monad.IO.Class (MonadIO, liftIO )
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.State.Strict (modify, get)
-import Network.Protocol.Snmp (OID)
-import Network.Protocol.Snmp.AgentX.MIBTree.Types
--- import Data.Label.Monadic (gets, puts, modify)
-import Control.Applicative hiding (empty)
+import Control.Monad.IO.Class (MonadIO)
+-- import Control.Monad.Trans.Class (lift)
+-- import Network.Protocol.Snmp (OID)
+import Network.Protocol.Snmp.AgentX.MIBTree.Types (ZipperM, Zipper)
+import qualified Network.Protocol.Snmp.AgentX.MIBTree.Types as Z
+import Data.Label.Monadic (modify, gets, puts)
+import Control.Applicative ((<$>))
 -- import Control.Concurrent.MVar
-import Control.Monad (void)
+import Data.Maybe (fromMaybe)
 
 
--- empty :: (Monad m, MonadIO m, Functor m) => OID -> Zipper m
--- empty oi = Zipper 
+goNext ::(Monad m, MonadIO m, Functor m) =>  ZipperM m a ()
+goNext = modify Z.zipper $ change Z.goNext  
 
-addNext :: (Monad m, MonadIO m, Functor m) => MTree (PVal m)  -> ZipperM m ()
-addNext x = modify $ \st -> st { moving = Next x : moving st }
+change :: (a -> Maybe a) -> a -> a
+change f x = fromMaybe x (f x)
 
-addLevel :: (Monad m, MonadIO m, Functor m) => MTree (PVal m)  -> ZipperM m ()
-addLevel x = modify $ \st -> st { moving = Level x : moving st }
+move :: (Monad m, MonadIO m, Functor m) => (Zipper a -> Maybe (Zipper a)) -> ZipperM m a ()
+move x = modify Z.zipper $ change x
 
-goNext ::(Monad m, MonadIO m, Functor m) =>  ZipperM m Bool
-goNext = goNext' =<< zipper <$> get
-  where
-  goNext' Empty = return False
-  goNext' (Node i next level) = do
-      modify $ \st -> st { zipper = next }
-      addNext (Node i Empty level)
-      return True
-  goNext' (Leaf i a next) = do
-      modify $ \st -> st { zipper = next }
-      addNext (Leaf i a Empty)
-      return True
+goLevel :: (Monad m, MonadIO m, Functor m) => ZipperM m a ()
+goLevel = move Z.goLevel
 
-goLevel :: (Monad m, MonadIO m, Functor m) => ZipperM m Bool
-goLevel = goLevel' =<< zipper <$> get
-  where
-  goLevel' Empty = return False
-  goLevel' Leaf{} = return False
-  goLevel' (Node i next level) = do
-      modify $ \st -> st { zipper = level }
-      addLevel (Node i next Empty)
-      return True
-
-goBack :: (Monad m, MonadIO m, Functor m) => ZipperM m Bool
-goBack = goBack' =<< moving <$> get
-  where
-  goBack' [] = return False
-  goBack' (Next (Node i Empty level) : bs) = do
-      z <- zipper <$> get
-      modify $ \st -> st { zipper = Node i z level
-                        , moving = bs
-                        }
-      return True
-  goBack' (Next (Leaf i a Empty) : bs) = do
-      z <- zipper <$> get
-      modify $ \st -> st { zipper = Leaf i a z
-                        , moving = bs
-                        }
-      return True
-  goBack' (Level (Node a next Empty) : bs) = do
-      z <- zipper <$> get
-      modify $ \st -> st { zipper = Node a next z 
-                        , moving = bs
-                        }
-      return True
-  goBack' _ = fail "bad case goBack'"
+goBack :: (Monad m, MonadIO m, Functor m) => ZipperM m a ()
+goBack = move Z.goBack
 
 
-top :: (Monad m, MonadIO m, Functor m) => ZipperM m ()
-top = top' =<< moving <$> get
-  where
-  top' [] = return ()
-  top' _ = (void goBack) >> top
+top :: (Monad m, MonadIO m, Functor m) => ZipperM m a ()
+top = modify Z.zipper Z.top >> puts Z.oid []
 
 
-goUp :: (Monad m, MonadIO m, Functor m) => ZipperM m  Bool
-goUp = goUp' =<< moving <$> get
-  where
-  goUp' [] = return False
-  goUp' (Next (Leaf i a Empty) : bs) = do
-      z <- zipper <$> get
-      modify $ \st -> st { zipper = Leaf i a z
-                        , moving = bs
-                        }
-      goUp
-  goUp' (Next (Node a Empty level) : bs) = do
-      z <- zipper <$> get
-      modify $ \st -> st { zipper = Node a z level
-                        , moving = bs
-                        }
-      goUp
-  goUp' (Level (Node a next Empty) : bs) = do
-      z <- zipper <$> get
-      modify $ \st -> st { zipper = Node a next z 
-                        , moving = bs
-                        }
-      return True
-  goUp' _ = fail "bad case goUp'"
+goUp :: (Monad m, MonadIO m, Functor m) => ZipperM m a ()
+goUp = move Z.goUp
 
-focus :: (Monad m, MonadIO m, Functor m) => ZipperM m ()
-focus = do
-    _o <- oid <$> get
-    t <- zipper <$> get
-    case t of
-        Leaf _ (Read fun) _ -> do
-            y <-  lift fun
-            liftIO $ print y
+
+cursor :: (Monad m, MonadIO m, Functor m) => ZipperM m a (Maybe Integer)
+cursor = Z.cursor <$> gets Z.zipper 
+{--
+focus :: (Monad m, MonadIO m, Functor m, Show a) => ZipperM m a a
+focus = gets Z.zipper
+--}
              
-
-isEmpty :: (Monad m, MonadIO m, Functor m) => ZipperM m Bool
-isEmpty = isEmpty' . zipper <$> get 
-  where
-  isEmpty' :: MTree (PVal m)  -> Bool 
-  isEmpty' Empty = True
-  isEmpty' _     = False
-
-isTop :: (Monad m, MonadIO m, Functor m) => ZipperM m Bool
-isTop = null . moving <$> get 
 
--- src/Network/Protocol/Snmp/AgentX/MIBTree/Types.hs
+++ src/Network/Protocol/Snmp/AgentX/MIBTree/Types.hs
@@ -10,6 +10,9 @@ module Network.Protocol.Snmp.AgentX.MIBTree.Types where
 
 import Control.Monad.State.Strict hiding (gets, modify)
 -- import Control.Concurrent.MVar
+import Data.Maybe (fromJust)
+import Data.Monoid ((<>))
+import Data.Label
 
 import Data.Map.Strict (Map)
 import Network.Protocol.Snmp (Value(..), OID)
@@ -26,22 +29,103 @@ data PVal m = Read
             , undoSetAIO     :: Maybe Context -> Value -> m UndoError
             }
 
+instance (Monad m, MonadIO m) => Show (PVal m) where
+    show Read{} = "Read Value"
+    show ReadWrite{} = "ReadWrite Value"
+
+cursor :: Zipper a -> Maybe Integer
+cursor ((Node i _ _), _) = Just i
+cursor ((Leaf i _ _), _) = Just i
+cursor (Empty       , _) = Nothing
+
 data MTree a = Node Integer   (MTree a) (MTree a)
              | Leaf Integer a (MTree a) 
              | Empty
 
+testTree :: MTree String
+testTree = Node 0 Empty 
+  (Node 11 
+    (Leaf 12 "1 level" Empty) 
+    (Node 21 
+      (Leaf 22 "2 level" 
+      (Leaf 23 "2 level" 
+      (Leaf 24 "2 level" 
+      Empty)))
+        Empty)
+  )
+
+testZipper :: Zipper String
+testZipper = (testTree, [])
+
 data Move a = Next (MTree a) 
             | Level (MTree a)
 
+instance Show a => Show (Move a) where
+    show (Next x) = "\nNext " ++ show x
+    show (Level x) = "\nLevel " ++ show x
+
 type Moving a = [Move a]
 
-data Zipper m = Zipper
-  { zipper        :: MTree (PVal m)
-  , moving        :: Moving (PVal m)
-  , oid           :: OID
-  , moduleOID     :: OID
+type Zipper a = (MTree a, Moving a)
+
+data Storage a = Storage
+  { _zipper        :: Zipper a
+  , _oid           :: OID
+  , _moduleOID     :: OID
   }
 
-type ZipperM m = StateT (Zipper m) m
+mkLabel ''Storage
+
+type ZipperM m a = StateT (Storage a) m
+
+
+toZipper :: MTree a -> Zipper a 
+toZipper t = (t, [])
+
+attach :: MTree a -> Zipper a -> Zipper a 
+attach t (_, bs) = (t, bs)
+
+goNext :: Zipper a -> Maybe (Zipper a)
+goNext (Empty, _) = Nothing
+goNext (Node _ Empty _, _) = Nothing
+goNext (Leaf _ _ Empty, _) = Nothing
+goNext (Leaf i v next, bs) = Just (next, Next (Leaf i v Empty):bs)
+goNext (Node i next link, bs) = Just (next, Next (Node i Empty link):bs)
+
+goLevel :: Zipper a -> Maybe (Zipper a)
+goLevel (Empty, _) = Nothing
+goLevel (Leaf{}, _) = Nothing
+goLevel (Node _ _ Empty, _) = Nothing
+goLevel (Node i next link, bs) = Just (link, Level (Node i next Empty):bs)
+
+goBack :: Zipper a -> Maybe (Zipper a)
+goBack (_, []) = Nothing
+goBack (t, Next (Leaf i v Empty):bs) = Just (Leaf i v t, bs)
+goBack (t, Next (Node i Empty link):bs) = Just (Node i t link, bs)
+goBack (t, Level (Node i next Empty):bs) = Just (Node i next t, bs)
+goBack _ = Nothing
+
+goUp :: Zipper a -> Maybe (Zipper a)
+goUp (_, []) = Nothing
+goUp (t, Next (Leaf i v Empty):bs) = goUp (Leaf i v t, bs)
+goUp (t, Next (Node i Empty link):bs) = goUp (Node i t link, bs)
+goUp (t, Level (Node i next Empty):bs) = Just (Node i next t, bs)
+goUp _ = Nothing
+
+top :: Zipper a -> Zipper a
+top (t,[]) = (t,[])  
+top z = top (fromJust $ goBack z)
+
+instance Show a => Show (MTree a) where
+    show f = unlines $ drawLevel f
+      where
+        drawLevel Empty = []
+        drawLevel (Node i next link) = ("Object " <> show i <> " " ) : (drawSubtree next link)
+        drawLevel (Leaf i v next) = ("ObjectType " <> show i <> " " <> show v <> " " ) : (drawSubtree next Empty)
+        
+        drawSubtree next link = (shift "`- " " | " (drawLevel link)) <> drawLevel next
+    
+        shift first rest = zipWith (++) (first : repeat rest)
+
 
 
