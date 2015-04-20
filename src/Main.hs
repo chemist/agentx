f4798d5cea4fb677de0280ba34d49c73a243c84f
Alexey Smirnov <chemistmail@gmail.com>
2015-04-20 17:55:55 +0300

    dyn

--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,13 @@
 module Main where
 
 import Network.Protocol.Snmp.AgentX 
-import Network.Info
 import qualified Data.ByteString as BS
 import Data.ByteString (ByteString)
-import qualified Network.Info as NI
-import Data.ByteString.Char8 (pack)
-import Data.Monoid ((<>))
 import Data.Fixed (div')
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Control.Applicative ((<$>))
 import Control.Monad.State
 import Data.IORef
-import GHC.Int (Int32)
 
 
 
@@ -54,35 +49,12 @@ rwi io = rwValue readV commit test undo
 dynTree :: IORef Value -> Update
 dynTree i = Update $ do
     Integer count <- liftIO $ readIORef i
-    let first = mkObjectType 0 "dyn" "count" Nothing (rwi i)
-    return $ first : map (\x -> mkObject (fromIntegral x + 1) "dyn" ("count" ++ show (x + 1)) (Just (subTree x))) [0 .. count] 
-
-subTree :: Int32 -> Update 
-subTree i = Update (return ls)
-  where
-    ls =
-        [ mkObjectType 0 ("count" ++ show (i + 1)) "first" Nothing (str "first")
-        , mkObjectType 1 ("count" ++ show (i + 1)) "second" Nothing (str "second")
-        ]
-
-interfaces :: Update 
-interfaces = Update ifaces
-    where 
-    ifaces :: (Monad m, MonadIO m, Functor m) => m [MIB]
-    ifaces = do
-        nx <- liftIO $ getNetworkInterfaces
-        let xs = zip [0 .. fromIntegral $ length nx - 1] nx
-            indexes = flip map xs $ \(i,_) -> mkObjectType i "indexes" "index" Nothing (rsValue . Integer . fromIntegral $ i) 
-            names = flip map xs $ \(i, o) -> mkObjectType i "names" "name" Nothing (rsValue . String . pack . NI.name $ o) 
-            ipv4s = flip map xs $ \(i, o) -> mkObjectType i "ipv4s" "ipv4" Nothing (rsValue . String . pack . show . NI.ipv4 $ o) 
-            ipv6s = flip map xs $ \(i, o) -> mkObjectType i "ipv6s" "ipv6" Nothing (rsValue . String . pack . show . NI.ipv6 $ o) 
-            macs = flip map xs $ \(i, o) -> mkObjectType i "macs" "mac" Nothing (rsValue . String . pack . show . NI.mac $ o) 
-        return $ 
-              (mkObject 0 "interfaces" "indexes" Nothing : indexes) <>
-              (mkObject 1 "interfaces" "names"   Nothing : names)   <>
-              (mkObject 2 "interfaces" "ipv4s"   Nothing : ipv4s)   <>
-              (mkObject 3 "interfaces" "ipv6s"   Nothing : ipv6s)   <>
-              (mkObject 4 "interfaces" "macs"    Nothing : macs) 
+    return $ concatMap fun [0 .. count]
+    where
+    fun x = [ mkObject (fromIntegral x) "trees" ("tree" ++ show x) Nothing
+            , mkObjectType 0 ("tree" ++ show x) "abr" Nothing (str "abr")
+            , mkObjectType 1 ("tree" ++ show x) "abrvalg" Nothing (str "abrvalg")
+            ]
 
 ver :: Maybe Context
 ver = Just "version"
@@ -94,8 +66,9 @@ simpleTree m i =
       , mkObjectType 1 "about" "version" Nothing $ rsValue (String "0.0.1")
       , mkObjectType 1 "about" "version" ver $ rsValue (String "Alpha")
       , mkObjectType 2 "about" "comment" Nothing (rws m) 
-      , mkObject 1 "Fixmon" "dyn" (Just (dynTree i))
-      -- , mkObject 2 "Fixmon" "interfaces" (Just interfaces)
+      , mkObject 1 "Fixmon" "dyn" Nothing
+      , mkObjectType 0 "dyn" "counter" Nothing (rwi i)
+      , mkObject 1 "dyn" "trees" (Just $ dynTree i)
       ]
 
 tree :: IO [MIB]
